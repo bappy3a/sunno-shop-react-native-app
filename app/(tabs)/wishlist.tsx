dfd77865d5f171af70fddf10c0e72588f79ec74e
Ahmed Bappy <bappy3a@gmail.com>
2026-07-16 01:54:40 +0600

    update now

---
+++ app/(tabs)/wishlist.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export class Wishlist extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Wishlist</Text>
+      </View>
+    )
+  }
+}
+
+export default Wishlist
