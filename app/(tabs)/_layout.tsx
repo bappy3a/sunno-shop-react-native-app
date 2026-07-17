418434bf136bd4a76cfecbc99ec981da7073dc9b
Ahmed Bappy <bappy3a@gmail.com>
2026-07-18 01:47:12 +0600

    slider add done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,64 +1,51 @@
-import Feather from '@expo/vector-icons/Feather';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import { Tabs } from 'expo-router';
+import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 import React from 'react';
-import { View } from 'react-native';
-
-import { colors, components } from '@/constants/theme';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-
-const tabBar = components.tabBar;
 
 const TabLayout = () => {
 
-    const insets = useSafeAreaInsets();
-
-    const TabIcon = ({ focused, iconName, icon }: { focused: boolean, iconName: any, icon: string }) => {
-        const IconComponent = icon === 'Feather' ? Feather : FontAwesome5;
-        const iconSize = icon === 'Feather' ? 24 : 24;
-
-        return (
-            <View className={`h-11 w-11 flex items-center justify-center rounded-full ${focused ? 'bg-surface' : ''}`}>
-                <IconComponent
-                    name={iconName}
-                    size={iconSize}
-                    color={focused ? colors.primary : "rgba(255, 255, 255, 0.5)"}
-                />
-            </View>
-        );
-    }
-
     return (
-        <Tabs screenOptions={{
-            headerShown: false,
-            tabBarShowLabel: false,
-            tabBarStyle: {
-                position: 'absolute',
-                bottom: insets.bottom > 0 ? insets.bottom - 12 : 8,
-                height: tabBar.height,
-                marginHorizontal: tabBar.horizontalInset,
-                borderRadius: tabBar.radius,
-                backgroundColor: colors.primary,
-                borderTopWidth: 0,
-                elevation: 0,
-            },
-            tabBarItemStyle: {
-                justifyContent: 'center',
-                alignItems: 'center',
-            },
-            tabBarIconStyle: {
-                width: tabBar.iconFrame,
-                height: tabBar.iconFrame,
-                justifyContent: 'center',
-                alignItems: 'center',
-            }
-        }}>
-            <Tabs.Screen name="index" options={{ title: "Home", tabBarIcon: ({ focused }) => (<TabIcon focused={focused} iconName="home" icon="Feather" />) }} />
-            <Tabs.Screen name="wishlist" options={{ title: "Wishlist", tabBarIcon: ({ focused }) => (<TabIcon focused={focused} iconName="heart" icon="Feather" />) }} />
-            <Tabs.Screen name="cart" options={{ title: "Cart", tabBarIcon: ({ focused }) => (<TabIcon focused={focused} iconName="shopping-cart" icon="Feather" />) }} />
-            <Tabs.Screen name="message" options={{ title: "Message", tabBarIcon: ({ focused }) => (<TabIcon focused={focused} iconName="message-square" icon="Feather" />) }} />
-            <Tabs.Screen name="account" options={{ title: "Account", tabBarIcon: ({ focused }) => (<TabIcon focused={focused} iconName="user-circle" icon="FontAwesome5" />) }} />
-        </Tabs>
+        <NativeTabs tintColor="#3C5A5D">
+            <NativeTabs.Trigger name="index">
+                <Label>Home</Label>
+                <Icon sf={{
+                    default: 'house',
+                    selected: 'house.fill',
+                }} drawable="custom_android_drawable" />
+            </NativeTabs.Trigger>
+
+            <NativeTabs.Trigger name="wishlist">
+                <Label>Wishlist</Label>
+                <Icon sf={{
+                    default: 'heart',
+                    selected: 'heart.fill',
+                }} drawable="custom_android_drawable" />
+            </NativeTabs.Trigger>
+
+            <NativeTabs.Trigger name="cart">
+                <Label>Cart</Label>
+                <Icon sf={{
+                    default: 'cart',
+                    selected: 'cart.fill',
+                }} drawable="custom_android_drawable" />
+            </NativeTabs.Trigger>
+
+            <NativeTabs.Trigger name="message">
+                <Label>Message</Label>
+                <Icon sf={{
+                    default: 'message',
+                    selected: 'message.fill',
+                }} drawable="custom_android_drawable" />
+            </NativeTabs.Trigger>
+
+            <NativeTabs.Trigger name="account">
+                <Label>Account</Label>
+                <Icon sf={{
+                    default: 'person',
+                    selected: 'person.fill',
+                }} drawable="custom_android_drawable" />
+            </NativeTabs.Trigger>
+
+        </NativeTabs>
     )
 }
 
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,26 @@
+import HomeSlider from '@/components/homeSlider';
+import Octicons from '@expo/vector-icons/Octicons';
 import React from 'react';
-import { Text, View } from 'react-native';
-
+import { ScrollView, TextInput, TouchableOpacity, View } from 'react-native';
 const Index = () => {
   return (
-    <View>
-      <Text>Index</Text>
-    </View>
+    <ScrollView className='h-full bg-white p-4'>
+      <View className='flex flex-row gap-2'>
+        <View className='w-10/12'>
+          <TextInput className='text-brand bg-[#f6f6f6] h-[50px] rounded-lg p-2' placeholder='Search Your products' placeholderTextColor={'#3C5A5D'} />
+        </View>
+        <View className='w-2/12'>
+          <TouchableOpacity className=' bg-brand flex justify-center items-center h-[50px] rounded-lg'>
+            <Octicons name="search" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View className='w-full mt-4'>
+        <HomeSlider />
+      </View>
+
+    </ScrollView>
   )
 }
 
